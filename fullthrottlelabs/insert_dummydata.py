d792d7d2d3bea9e8a53c4b0ea9862c22046b99ac
dineshbhupathi <bupathidinesh95@gmail.com>
2020-04-02 13:52:03 +0530

    updated with script to insert data into database

--- fullthrottlelabs/insert_dummydata.py
+++ fullthrottlelabs/insert_dummydata.py
@@ -1,42 +1,52 @@
 import sqlite3
 
-def insert_users_into_table(real_name, tz,start_time, end_time,activity_periods):
+def insert_users_into_table(real_name, tz):
     try:
         connection = sqlite3.connect('db.sqlite3')
         cursor = connection.cursor()
-        print("Connected to SQLite")
-
         users_insert_with_param = """INSERT INTO usermanagement_User
-                          (real_name, tz) 
+                          (real_name, tz)
                           VALUES (?, ?);"""
-
-        users_activity_insert_with_param = """INSERT INTO usermanagement_ActivityPeriod
-                          (start_time, end_time) 
-                          VALUES (?, ?);"""
-        activityperiod_inser =  """SELECT * FROM usermanagement_activityperiod
-                          ;"""
         data = (real_name,tz)
-        activity_data = (start_time,end_time)
-        # a  = (activity_periods)
         cursor.execute(users_insert_with_param, data)
-        cursor.execute(users_activity_insert_with_param,activity_data)
-        cursor.execute(activityperiod_inser)
         connection.commit()
-        print("Python Variables inserted successfully into SqliteDb_developers table")
-
         cursor.close()
 
     except sqlite3.Error as error:
-        print("Failed to insert Python variable into sqlite table", error)
+        print("Failed to insert variables into sqlite table", error)
     finally:
         if (connection):
             connection.close()
             print("The SQLite connection is closed")
 
 
-insert_users_into_table('ajay','Asia/Kolkata','2020-03-20 08:00','2020-03-30 12:00',1)
-insert_users_into_table('harish','Asia/kolkata','2020-03-25 10:00','2020-03-31 01:00',2)
-
-###########################################################################
+insert_users_into_table('bhupathi','America/Los_Angeles')
+insert_users_into_table('ajay','Asia/kolkata')
 
 
+#########################################################################################################
+
+def insert_activity_periods_into_table(start_time, end_time,activity_periods):
+    try:
+        connection = sqlite3.connect('db.sqlite3')
+        cursor = connection.cursor()
+        users_activity_insert_with_param = """INSERT INTO usermanagement_ActivityPeriod
+                          (start_time, end_time,activity_periods_id)
+                          VALUES (?, ?,?);"""
+        activity_data = (start_time,end_time,activity_periods)
+        cursor.execute(users_activity_insert_with_param,activity_data)
+        connection.commit()
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Failed to insert variables into sqlite table", error)
+    finally:
+        if (connection):
+            connection.close()
+            print("The SQLite connection is closed")
+
+
+insert_activity_periods_into_table('2020-03-16 12:00','2020-03-28 13:00',2)
+insert_activity_periods_into_table('2020-03-15 01:00','2020-03-22 12:00',1)
+insert_activity_periods_into_table('2020-03-18 10:00','2020-03-26 08:00',2)
+insert_activity_periods_into_table('2020-03-16 07:00','2020-03-28 18:00',1)
